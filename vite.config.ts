432ef9888942c2dd8b76b8ced2bb9cc6f427d16f
phiresky <phireskyde+git@gmail.com>
2024-10-10 18:12:50 +0200

    o

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
+  base: "/reyers-digging-boardgame/",
   plugins: [
     remix({
       ssr: false,
